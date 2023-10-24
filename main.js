b5064260e00ebe3616d633109e70cc0f9a5935d5
Prekshagupta99 <prekshamanglagupta@gmail.com>
2023-10-25 00:27:13 +0530

    feat: added bg changing property

---
+++ main.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click',function(){
+    document.querySelector('.container').style.backgroundColor='pink'
+    //document.querySelector('body').classList.add('bg-change');
+});
+
