336dc6c4c68581805c4187eb00982ce65c91717b
Prekshagupta99 <prekshamanglagupta@gmail.com>
2023-10-25 12:46:27 +0530

    added random colour function

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const btn = document.querySelector('.btn');
 
-btn.addEventListener('click',function(){
-    document.querySelector('.container').style.backgroundColor='pink'
-    //document.querySelector('body').classList.add('bg-change');
-});
+const setBg = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+}
+
+// btn.addEventListener('click',function(){
+//     document.querySelector('.container').style.backgroundColor(randColor);
+//     //document.querySelector('body').classList.add('bg-change');
+// });
+
+btn.addEventListener('click', setBg);
+setBg();
 
