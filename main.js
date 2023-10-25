a47d57c36c2b6f98758a7711fd4947e0f6a5623a
Prekshagupta99 <prekshamanglagupta@gmail.com>
2023-10-25 17:48:45 +0530

    application form

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 const btn = document.querySelector('.btn');
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const phoneInput = document.querySelector('#phone');
 
-const setBg = () => {
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    document.body.style.backgroundColor = "#" + randomColor;
+btn.addEventListener('click', onSubmit);
+function onSubmit(){
+    if(nameInput ==' ' || emailInput == ' ' || phoneInput == ' '){
+        document.querySelector('.container').style.backgroundColor='red';
+    }else{
+        document.querySelector('.container').style.backgroundColor='green';
+    }
 }
 
-// btn.addEventListener('click',function(){
-//     document.querySelector('.container').style.backgroundColor(randColor);
-//     //document.querySelector('body').classList.add('bg-change');
-// });
-
-btn.addEventListener('click', setBg);
-setBg();
 
